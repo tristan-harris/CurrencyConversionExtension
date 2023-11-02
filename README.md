a720f5ae64174eeb39b6f5db56d4968c99addc61
BudgieKnight <budgieknight@gmail.com>
2023-11-02 12:30:09 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Currency Conversion Chrome Extension
+
+Adds a floating window specified pages to easily convert between currencies. Uses freecurrencyapi.com for data.
+
+---
+
+Credits icon from ([game-icons.net](https://game-icons.net/))
---
+++ content/main.js
@@ -0,0 +1,8 @@
+console.log("CURRENCY");
+
+document.body.insertAdjacentHTML("beforeend",`
+    <div class="currency-converter-popup-div">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit qui, adipisci omnis, impedit maiores labore nobis cum animi tempore quas nulla veritatis non perferendis mollitia ipsam voluptatibus officiis aliquid veniam!</p>
+    </div>
+`
+);
