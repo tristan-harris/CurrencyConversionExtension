546a4ae108144785acc6768ccc6fe3002a42a728
Tristan <tristan.harris520@gmail.com>
2023-11-04 17:16:33 +1100

    fixed floating popup css and added local storage

--- content/main.js
+++ content/main.js
@@ -8,54 +8,113 @@ let currencyDataObject = {};
 // when currency data was last requested (unix time)
 let requestTimestamp = 0;
 
-let currentTimestamp = Math.floor(Date.now() / 1000); // seconds
+let currentTimestamp = getCurrentTimestamp();
+let currentDomain = getCurrentDomain();
 
 let cceDiv;
+let popupHtml;
 
-fetch(chrome.runtime.getURL("/res/popup.html"))
-    .then((res) => res.text())
-    .then((html) => {
-        initialize(html);
-    })
-    .catch((e) => console.error(`${e}`));
+let font = new FontFace("Roboto", chrome.runtime.getURL("/res/font/Roboto-Medium.tff"));
+document.fonts.add(font);
+
+// execute functions in response to messages from window.js
+chrome.runtime.onMessage.addListener(messageObj => {
+    switch(messageObj.message) {
+        case "createPopup":
+            createPopup();
+            break;
+        case "destroyPopup":
+            destroyPopup();
+            break;
+        default:
+            console.error(`Unknown message "${message}" from window.js`);
+            break;
+    }
+});
 
 chrome.storage.local.get(
-    {requestTimestamp: currentTimestamp},
+    {requestTimestamp: 0, [currentDomain]: false, currencyData: null},
     (items) => 
     {
-        requestTimestamp = items.currentTimestamp;
-
-        if(currentTimestamp - requestTimestamp >= CURRENCY_DATA_UPDATE_INTERVAL) {
-            getCurrencyData();
+        // if popup is set to show for current web domain
+        if(items[currentDomain]) {
+            createPopup();
         }
+
+        requestTimestamp = items.requestTimestamp;
+
+        // if request timestamp not set yet
+        if(requestTimestamp == 0) {
+            chrome.storage.local.set({requestTimestamp: getCurrentTimestamp()});
+            updateCurrencyData(BASE_CURRENCY_CODE);
+        }
+        // if need to update with new currency data
+        else if(currentTimestamp - requestTimestamp >= CURRENCY_DATA_UPDATE_INTERVAL) {
+            updateCurrencyData(BASE_CURRENCY_CODE);
+        }
+
+        currencyDataObject = items.currencyData;
     }
 );
 
-function initialize(html) 
-{
-    let font = new FontFace("Roboto", chrome.runtime.getURL("/res/font/Roboto-Medium.tff"));
-    document.fonts.add(font);
-    
-    // getCurrencyData(BASE_CURRENCY_CODE);
-
-    document.body.insertAdjacentHTML("beforeend", html);
-    cceDiv = document.querySelector(".cce-popup-div");
-
-    cceDiv.querySelector("#cce-swap-currencies-arrow").src = chrome.runtime.getURL("/res/images/horizontal-flip.png");
-
-    // let convertButton = cceDiv.querySelector("#cce-convert-button");
-    // convertButton.addEventListener("click", onConvertButtonClicked);
+function getPopupHtml(_callback) {
+    fetch(chrome.runtime.getURL("/res/popup.html"))
+    .then((res) => res.text())
+    .then((html) => {
+        _callback(html);
+    })
+    .catch((e) => console.error(`${e}`));
 }
 
-function onConvertButtonClicked() {
+function createPopup() {
+    if(!document.querySelector(".cce-popup-div"))
+    {
+        getPopupHtml((popupHtml) => 
+        {
+            document.body.insertAdjacentHTML("beforeend", popupHtml);
+            cceDiv = document.querySelector(".cce-popup-div");
+
+            cceDiv.querySelector("#cce-base-currency-input-value").addEventListener("input", updateCurrencyCalculation);
+            cceDiv.querySelector("#cce-base-currency-input-code").addEventListener("change", updateCurrencyCalculation);
+            cceDiv.querySelector("#cce-comparison-currency-input-code").addEventListener("change", updateCurrencyCalculation);
+
+            cceDiv.querySelector("#cce-swap-currencies-arrow").src = chrome.runtime.getURL("/res/images/horizontal-flip.png");
+
+            cceDiv.querySelector("#cce-swap-currencies-arrow").addEventListener("click", onSwapArrowClicked);
+        });
+    }
+}
+
+function destroyPopup() {
+    if(cceDiv != undefined) {
+        cceDiv.remove();
+        cceDiv = undefined;
+    }
+}
+
+function onSwapArrowClicked() {
+    let baseCurrencySelect = cceDiv.querySelector("#cce-base-currency-input-code");
+    let comparisonCurrencySelect = cceDiv.querySelector("#cce-comparison-currency-input-code");
+
+    let baseCurrencySelectedIndex = baseCurrencySelect.selectedIndex;
+    baseCurrencySelect.selectedIndex = comparisonCurrencySelect.selectedIndex;
+    comparisonCurrencySelect.selectedIndex = baseCurrencySelectedIndex;
+
+    updateCurrencyCalculation();
+}
+
+function updateCurrencyCalculation() 
+{
+    // check that popup exists and currency data is available
+    if(cceDiv == undefined || currencyDataObject == null) {
+        return;
+    }
+
     let baseCurrencyCode = cceDiv.querySelector("#cce-base-currency-input-code").value;
     let comparisonCurrencyCode = cceDiv.querySelector("#cce-comparison-currency-input-code").value;
-
     let baseCurrencyValue = cceDiv.querySelector("#cce-base-currency-input-value").value;
     let comparisonCurrencyValue = 0;
 
-    console.log(baseCurrencyValue);
-    
     // first convert base currency to AUD value
     baseCurrencyValue /= currencyDataObject["data"][baseCurrencyCode];
 
@@ -66,11 +125,9 @@ function onConvertButtonClicked() {
     comparisonCurrencyValue = comparisonCurrencyValue.toFixed(2);
 
     cceDiv.querySelector("#cce-comparison-currency-input-value").value = comparisonCurrencyValue;
-
-    console.log(baseCurrencyValue);
 }
 
-function getCurrencyData(baseCurrencyCode) 
+function updateCurrencyData(baseCurrencyCode) 
 {
     // getting the values of all currencies compared to AUD so don't need to make multiple requests
     let allCurrencyCodes = "AUD%2CBRL%2CGBP%2CBGN%2CCAD%2CCNY%2CHRK%2CCZK%2CDKK%2CEUR%2CHKD%2CHUF%2CISK%2CINR%2CIDR%2CILS%2CJPY%2CMYR%2CMXN%2CNZD%2CNOK%2CPHP%2CPLN%2CRON%2CRUB%2CSGD%2CZAR%2CKRW%2CSEK%2CCHF%2CTHB%2CTRY%2CUSD"; // %2C = ','
@@ -85,9 +142,21 @@ function getCurrencyData(baseCurrencyCode)
             }
         })
         .then((data) => {
+            chrome.storage.local.set({requestTimestamp: getCurrentTimestamp(), currencyData: data});
             currencyDataObject = data;
         })
         .catch((error) => {
             console.error(error);
         });
+}
+
+function getCurrentTimestamp() {
+    return Math.floor(Date.now() / 1000); // seconds
+}
+
+function getCurrentDomain() {
+    let currentUrl = window.location.href;
+    currentUrl = currentUrl.replace("https://", "").replace("http://", "");
+    currentUrl = currentUrl.substring(0, currentUrl.indexOf("/"));
+    return currentUrl;
 }
--- window/window.js
+++ window/window.js
@@ -1,3 +1,13 @@
+const hideButton = document.querySelector("#cce-hide-button");
+const showButton = document.querySelector("#cce-show-button");
+
+const hideButtonColor = "rgb(225, 47, 47)";
+const showButtonColor = "rgb(112, 228, 77)";
+
+let websiteDomain = "";
+let selectedButton;
+
+// getting the active tab
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     if(tabs[0] != undefined) {
         let currentUrl = tabs[0].url;
@@ -5,12 +15,66 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         // if not chrome://, file:// etc
         if(currentUrl.startsWith("https://") || currentUrl.startsWith("http://")) 
         {
-            let websiteDomain = currentUrl.replace("https://", "").replace("http://", "");
-            websiteDomain = websiteDomain.substr(0, websiteDomain.indexOf("/"));
+            websiteDomain = currentUrl.replace("https://", "").replace("http://", "");
+            websiteDomain = websiteDomain.substring(0, websiteDomain.indexOf("/"));
 
             document.querySelector("#cce-current-website").textContent = websiteDomain;
-            document.querySelector("#cce-hide-button").removeAttribute("disabled");
-            document.querySelector("#cce-show-button").removeAttribute("disabled");
+
+            hideButton.removeAttribute("disabled");
+            showButton.removeAttribute("disabled");
+
+            hideButton.addEventListener("click", onHideButtonClicked);
+            showButton.addEventListener("click", onShowButtonClicked);
+
+            updateButtons();
         }
+    } else {
+        console.error("Cannot access tab information");
     }
 });
+
+function updateButtons() {
+    chrome.storage.local.get(
+        {[websiteDomain]: false}, // default value of false if websiteDomain not stored in chrome storage
+        (items) => {
+            if(items[websiteDomain]) {
+                selectedButton = showButton;
+            } else {
+                selectedButton = hideButton;
+            }
+            updateButtonColors();
+        }
+    );
+}
+
+function updateButtonColors() {
+    if(selectedButton == showButton) {
+        hideButton.setAttribute("style", "");
+        showButton.setAttribute("style", `background-color: ${showButtonColor}; border-radius: 2px;`);
+    } else {
+        hideButton.setAttribute("style", `background-color: ${hideButtonColor}; border-radius: 2px;`);
+        showButton.setAttribute("style", "");
+    }
+}
+
+function onHideButtonClicked() {
+    chrome.storage.local.set({[websiteDomain]: false});
+    selectedButton = hideButton;
+    updateButtonColors();
+    sendMessageToMain("destroyPopup");
+}
+
+function onShowButtonClicked() {
+    chrome.storage.local.set({[websiteDomain]: true});
+    selectedButton = showButton;
+    updateButtonColors();
+    sendMessageToMain("createPopup");
+}
+
+function sendMessageToMain(message) {
+    chrome.tabs.query({active: true, currentWindow: true}, 
+    (tabs) => {
+        console.log(tabs[0]);
+        chrome.tabs.sendMessage(tabs[0].id, {message: message});
+    });
+}
